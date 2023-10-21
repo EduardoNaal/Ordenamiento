c444679c49466665727fc52052e49305ce9c1aa4
Eduardo Javier Naal <131737602+EduardoNaal@users.noreply.github.com>
2023-10-20 18:10:24 -0600

    Add files via upload

---
+++ ordenamiento/BubbleSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static int[] bubbleSort(int[] arr) {
+        int aux;
+        boolean swap = true;
+        
+        while (swap) {
+            swap = false;
+            for (int i = 1; i < arr.length; i++) {
+                
+                if (arr[i - 1] > arr[i]) {
+                    
+                    swap = true;
+                    aux = arr[i];
+                    arr[i] = arr[i - 1];
+                    arr[i - 1] = aux;
+                    System.out.println("-" + Arrays.toString(arr));
+                }
+            }
+            
+            if (!swap) 
+                break;
+        }
+              
+        return arr;
+    }
+}
